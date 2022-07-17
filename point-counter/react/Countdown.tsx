dce8c323ead4791f34d74b4743659b601dfe8041
Ciro Carvalho <cirocj@protonmail.com>
2022-07-17 18:30:54 -0300

    Atualiza a front-store

---
+++ point-counter/react/Countdown.tsx
@@ -0,0 +1,48 @@
+//react/Countdown.tsx
+import React /*, {useState}*/ from 'react'
+// import { TimeSplit } from './typings/global'
+// import { tick, getTwoDaysFromNow } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
+
+interface CountdownProps {
+  targetDate: string
+}
+
+// const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+const CSS_HANDLES = ['countdown']
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+  // targetDate = DEFAULT_TARGET_DATE
+}) => {
+  // const [timeRemaining, setTime] = useState<TimeSplit>({
+  //   hours: '00',
+  //   minutes: '00',
+  //   seconds: '00'
+  // })
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  // tick(targetDate, setTime)
+
+  return (
+    <div className={`${handles.countdown} c-muted-1 db tc`}>
+      <h5>{ `Seus Pontos :${500}` }</h5>
+    </div>
+  )
+}
+
+Countdown.schema = {
+  title: 'editor.countdown.title',
+  description: 'editor.countdown.description',
+  type: 'object',
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
+  },
+}
+
+export default Countdown
